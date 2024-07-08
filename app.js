9ab5c16e49acb634445fb13580d172804aa19a55
tongjp <tech@xmyy.com>
2024-07-08 11:35:08 +0800

    modify url

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ document.addEventListener('DOMContentLoaded', async () => {
     const ipInput = document.getElementById('ipInput');
     const lookupBtn = document.getElementById('lookupBtn');
     const loadingIndicator = document.getElementById('loadingIndicator');
-    const proxyServer = "https://eb331b3.r26.cpolar.top"
+    const proxyServer = "tcp://12.tcp.cpolar.top:13129"
     try {
         const response = await fetch('https://ipinfo.io/ip');
         const ip = await response.text();
