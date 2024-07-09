5b5940003a2f5d7fc3cafecfb38b4e74bc8da137
tongjp <tech@xmyy.com>
2024-07-09 21:52:40 +0800

    modify url

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ document.addEventListener('DOMContentLoaded', async () => {
         const response = await fetch('https://ipinfo.io/ip');
         const ip = await response.text();
         resultDiv.innerHTML = `您的当前IP是: ${ip}`;
-        const ipDetailsResponse = await fetch(`https://ip.900cha.com/${ip}.html`);
+        const ipDetailsResponse = await fetch(`https://zh-hans.ipshu.com/ipv4/${ip}`);
         if (!ipDetailsResponse.ok) {
             throw new Error('Failed to fetch IP details.');
         }
