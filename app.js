eb2b7451518a8ac5ed028e8a149bcf4bb9e22ce2
tongjp <tech@xmyy.com>
2024-07-09 21:33:45 +0800

    modify url

--- app.js
+++ app.js
@@ -3,12 +3,12 @@ document.addEventListener('DOMContentLoaded', async () => {
     const ipInput = document.getElementById('ipInput');
     const lookupBtn = document.getElementById('lookupBtn');
     const loadingIndicator = document.getElementById('loadingIndicator');
-    const proxyServer = "http://20b94c01.r11.cpolar.top"
+    const proxyServer = "https://www.ip138.com/iplookup.php"
     try {
         const response = await fetch('https://ipinfo.io/ip');
         const ip = await response.text();
         resultDiv.innerHTML = `您的当前IP是: ${ip}`;
-        const ipDetailsResponse = await fetch(`${proxyServer}/proxy/query?ip=${ip}`);
+        const ipDetailsResponse = await fetch(`${proxyServer}?ip=${ip}`);
         if (!ipDetailsResponse.ok) {
             throw new Error('Failed to fetch IP details.');
         }
